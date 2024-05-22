58443e8042fcdefafb7f5d6d11aa6c335d1e4cf5
Karan Palan <karanpalan007@gmail.com>
2024-05-22 10:27:38 +0530

    completed till comparison in js

---
+++ basics/1. variables.js
@@ -0,0 +1,5 @@
+const accountId = 4545446 // can't be changed
+let accountEmail = "karanpalan007@gmail.com" // let is used over var because of issues in block scope and functional scope
+var accountPassword = "12341524"
+
+console.table([accountId,accountEmail,accountPassword])
